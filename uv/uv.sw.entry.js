17bfb11d13f4e583fc8a0275fdba126a9794435b
Monts0-32 <brooksmonty87@gmail.com>
2026-05-19 23:03:28 +0100

    Clean up unused imports in uv.sw.entry.js
    Removed unused imports and constants related to BareMux.

--- uv/uv.sw.entry.js
+++ uv/uv.sw.entry.js
@@ -2,9 +2,6 @@ importScripts('/uv/uv.bundle.js');
 importScripts('/uv/uv.config.js');
 importScripts('/uv/uv.sw.js');
 
-// Tell bare-mux where the bare server is
-const { setTransport, EpoxyClient } = BareMux;
-
 const sw = new UVServiceWorker();
 
 self.addEventListener('fetch', event => {
