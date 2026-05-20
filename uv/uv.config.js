c23e8fdd64a128970a0d5c7283e3b0e410fe5967
Monts0-32 <brooksmonty87@gmail.com>
2026-05-20 06:39:02 +0100

    Update UV service prefix and bare URL

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,9 +1,9 @@
 self.__uv$config = {
-    prefix: '/service/',
-    bare: 'https://ada-app-production.up.railway.app/bare/',
+    prefix: '/uv/service/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
+    client: '/uv/uv.client.js',
     bundle: '/uv/uv.bundle.js',
     config: '/uv/uv.config.js',
     sw: '/uv/uv.sw.entry.js',
